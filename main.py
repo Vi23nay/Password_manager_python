08d29c8483e81cf430301d213eab2cd7c18bfee3
Vinay Singh <vi23nay.vs@gmail.com>
2021-08-15 15:42:28 +0530

    added Themes

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
+# Password Generator Project
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -23,16 +26,16 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    new_data={
-        website:{
-            "email":email,
-            "password":password,
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
         }
     }
     if len(website) == 0 or len(password) == 0:
@@ -44,7 +47,7 @@ def save():
                 data = json.load(data_file)
         except FileNotFoundError:
             with open("data.json", "w") as data_file:
-                json.dump(new_data,data_file,indent=4)
+                json.dump(new_data, data_file, indent=4)
         else:
             # updating new data (or adding new data)
             data.update(new_data)
@@ -56,30 +59,23 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
-#------------------------SEARCHING CREDENTIALS--------------------------#
+
+# ------------------------SEARCHING CREDENTIALS--------------------------#
 
 def searching_data():
-    website_name=website_entry.get()
+    website_name = website_entry.get()
     try:
         with open("data.json", "r") as file:
-            data=json.load(file)
+            data = json.load(file)
     except FileNotFoundError:
         messagebox.showerror(title="Error", message="No DataFile found")
     else:
         if website_name in data:
-            email=data[website_name]["email"]
-            password=data[website_name]["password"]
-            messagebox.showinfo(title=website_name,message=f"Email: {email}\nPassword: {password}")
+            email = data[website_name]["email"]
+            password = data[website_name]["password"]
+            messagebox.showinfo(title=website_name, message=f"Email: {email}\nPassword: {password}")
         else:
             messagebox.showerror(title="Error", message="No details for this website exists!!")
-
-
-
-
-
-
-
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -88,12 +84,12 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200, width=200)
+canvas = Canvas(height=200, width=200,highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
@@ -101,23 +97,48 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#Entries
+# Entries
 website_entry = Entry(width=20)
-website_entry.grid(row=1, column=1, columnspan=2,sticky="EW")
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
 website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2,sticky="EW")
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 email_entry.insert(0, "ma23hender@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1,sticky="EW")
+password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2,sticky="EW")
+generate_password_button.grid(row=3, column=2, sticky="EW")
 
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2,sticky="EW")
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
-search_button=Button(text="Search",command=searching_data)
-search_button.grid(row=1,column=2,sticky="EW")
+search_button = Button(text="Search", command=searching_data)
+search_button.grid(row=1, column=2, sticky="EW")
+
+#radiobutton-theme
+def dark():
+    window.config(bg="black")
+    canvas.config(bg="black")
+    website_label.config(bg="black",fg="white")
+    email_label.config(bg="black",fg="white")
+    password_label.config(bg="black",fg="white")
+
+def light():
+    window.config(bg="white")
+    canvas.config(bg="white")
+    website_label.config(bg="white", fg="black")
+    email_label.config(bg="white", fg="black")
+    password_label.config(bg="white", fg="black")
+#Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Dark", value=1, variable=radio_state, command=dark)
+radiobutton2 = Radiobutton(text="Light", value=2, variable=radio_state, command=light)
+radiobutton1.grid(row=6,column=1)
+radiobutton2.grid(row=6,column=2)
+
 window.mainloop()
+
+
+
